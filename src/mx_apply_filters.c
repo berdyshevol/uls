6a12438d599d2f7571808a9b709d4bf1ebd8119a
Yurii Makohon <ymakohon@e2r14p4.unit.ua>
2019-12-11 17:26:28 +0200

    Finaly cleaned all leacks in filters

--- src/mx_apply_filters.c
+++ src/mx_apply_filters.c
@@ -1,7 +1,7 @@
 #include "uls.h" 
 
 static void del_file(t_list **list, char *name);
-static void next_name(t_list **curr, t_list **prev, char **name, char *rm);
+// static void next_name(t_list **curr, t_list **prev, char **name, char *rm);
 
 void mx_apply_filters(t_App *app) {
     t_list *curr = app->cur_dir->list_attr;
@@ -23,7 +23,7 @@ void mx_apply_filters(t_App *app) {
 
 static void del_file(t_list **list, char *removed) {
     t_list *curr = *list;
-    t_list *prev = NULL;
+    // t_list *prev = NULL;
     char *name = ((t_attr *)(curr->data))->file_name;
 
     if (curr != NULL && mx_strcmp(name, removed) == 0) {
@@ -31,19 +31,18 @@ static void del_file(t_list **list, char *removed) {
         mx_free_node_data(curr);
         return;
     }
-    next_name(&curr, &prev, &name, removed);
-    if (curr == NULL)
-        return;
-    prev->next = curr->next;
-    free(curr);
+    // next_name(&curr, &prev, &name, removed);
+    // // if (curr == NULL)
+    // //     return;
+    // // prev->next = curr->next;
+    // printf("AAAAAAAAA\n");
 }
 
-
-static void next_name(t_list **curr, t_list **prev, char **name, char *rm) {
-    while (curr != NULL && mx_strcmp(*name, rm) != 0) {
-        (*prev) = (*curr);
-        (*curr) = (*curr)->next;
-        if ((*curr) != NULL)
-            *name = ((t_attr *)((*curr)->data))->file_name;
-    }
-}
+// static void next_name(t_list **curr, t_list **prev, char **name, char *rm) {
+//     while (curr != NULL && mx_strcmp(*name, rm) != 0) {
+//         (*prev) = (*curr);
+//         (*curr) = (*curr)->next;
+//         if ((*curr) != NULL)
+//             *name = ((t_attr *)((*curr)->data))->file_name;
+//     }
+// }
